098f03978f87508cbbb3b53372e227d7dced144f
Hesna Akay <hesnaakay@gmail.com>
2017-07-22 11:19:49 +0300

    first-commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            
+            return View(customers);
+        }
+
+        //GET:Customers/Details/1
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {id = 1, Name = "Hesna"},
+                new Customer {id = 2, Name = "Hakan"}
+            };
+        }
+
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+               new Movie {Name = "Edge of Tomorrow"},
+               new Movie {Name = "american history x"}
+            };
+            return View("ReadOnlyList", movies);
+        }
+
+        // GET: Movies/Random
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        
+    }
+}
